6d3904264d7458e6899c63817cbb6284fd2602f9
Timnasa <Halisimd212@gmail.com>
2024-12-11 00:14:32 +0300

    Update stickersearch.js

--- commandes/stickersearch.js
+++ commandes/stickersearch.js
@@ -33,7 +33,7 @@ async (dest, zk, commandeOptions) => {
 
     const stickerMess = new Sticker(gifUrl, {
       pack: packname,
-      author: 'HANS-MD',
+      author: 'TIMNASA-MD',
       type: StickerTypes.FULL,
       categories: ["🤩", "🎉"],
       id: "12345",
